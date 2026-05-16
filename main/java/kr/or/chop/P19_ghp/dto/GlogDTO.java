975e16fdf8502795f4c6c40f98586f616ddeb17e
홍세정 <blueat02@naver.com>
2026-05-16 16:30:51 +0900

    modelConfig 수정됨. P19_ghp 패키지 추가 돼야 함.

---
+++ main/java/kr/or/chop/P19_ghp/dto/GlogDTO.java
@@ -0,0 +1,33 @@
+package kr.or.chop.P19_ghp.dto;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class GlogDTO {
+	
+	private String glogId;
+	private Date glogDate;
+	private String glogWorker;
+	private int glogValue = -9999;
+	private String glogResult;
+	private	String glogAction;
+	private Date glogAdate;
+	private String glogImg;
+	private String glogMemo;
+	private Date glogCdate;
+	
+	private String ghpId;
+	private String ghpName;
+	private String ghpCategory;
+	private String ghpStandard;
+	private String ghpCheckMethod;
+	private String ghpCycle;
+	private String ghpWhId;
+	private String ghpWpId;
+	private int ghpMinValue = -9999;
+	private int ghpMaxvalue = -9999;
+	private String ghpUnit;
+	
+}
