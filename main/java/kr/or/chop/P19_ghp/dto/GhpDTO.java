31fa0ab0086f28fb4717833646adfd5a3cb5557f
홍세정 <blueat02@naver.com>
2026-05-18 10:41:04 +0900

    이용상 콱 씨

---
+++ main/java/kr/or/chop/P19_ghp/dto/GhpDTO.java
@@ -0,0 +1,17 @@
+package kr.or.chop.P19_ghp.dto;
+
+public class GhpDTO {
+	
+	String ghpId;
+	String ghpName;
+	String ghpCategory;
+	String ghpStandard;
+	String ghpCheckMethod;
+	String ghpCycle;
+	String ghpWhId;
+	String ghpWpId;
+	Integer ghpMinValue;
+	Integer ghpMaxValue;
+	String ghpUnit;
+	
+}
